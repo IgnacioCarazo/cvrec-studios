a3d7a30621897ebfa1572bb47ff489bd84e07720
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2025-02-27 17:53:23 -0600

    chore: change base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cvrec-studios/", // Ensure the base path is set to root
+  base: "/", // Ensure the base path is set to root
   server: {
     host: '0.0.0.0',
   },
